be01d576ecad6d6fa24bb2c2b14b225c27e516ce
sai <saikirankondapalli03@gmail.com>
2019-10-19 00:21:07 -0400

    Update TravellerApplicationMain.java

--- src/main/java/com/travellerapp/TravellerApplicationMain.java
+++ src/main/java/com/travellerapp/TravellerApplicationMain.java
@@ -1,4 +1,4 @@
-ackage com.travellerapp;
+package com.travellerapp;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
